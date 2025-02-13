ade281bfde1f09416a56f5863f20efc4d8f161c2
johnny1029 <jkste07@gmail.com>
2025-02-13 19:02:00 +0100

    remove corsheaders

--- djangocreator/settings.py
+++ djangocreator/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-        'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,8 +135,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 
 LOGOUT_REDIRECT_URL = '/login/'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",  
-    "http://127.0.0.1:8000",
-]
