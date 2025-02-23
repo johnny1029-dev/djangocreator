324aa725b43348d3fcd14d6b8cd54d073e477ffe
johnny1029 <jkste07@gmail.com>
2025-02-23 22:49:50 +0100

    something to work with later

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   const textarea = document.getElementById("editor-textarea");
 
+  function sweetAlert(){
+    (async () => {
+  
+    const { value: formValues } = await Swal.fire({
+      title: 'Multiple inputs',
+      html:
+        '<input id="swal-input1" class="swal2-input">' +
+        '<input id="swal-input2" class="swal2-input">',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          document.getElementById('swal-input1').value,
+          document.getElementById('swal-input2').value
+        ]
+      }
+    })
+  
+    if (formValues) {
+      Swal.fire(JSON.stringify(formValues))
+    }
+  
+    })()
+  }
+
+  sweetAlert();
+
   textarea.addEventListener("keydown", function (e) {
     if (e.key === "Tab") {
       e.preventDefault();
